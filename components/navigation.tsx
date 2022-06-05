ad6aa3a8f1903815a07c6f0c77eb3bc73bbefeab
Jonathan Maskew <jmaskew@purdue.edu>
2022-06-05 18:54:58 -0400

    initial file set up

---
+++ components/navigation.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+export default class Navigation extends Component<Props, State> {
+    state = {}
+
+    render() {
+        return (
+            <div>
+                <p>Jonathan Maskew</p>
+
+            </div>
+        )
+    }
+}
